f33f3758e552e402c8ac3f02b773ba48ea30c580
Ron Park <ronpark.dev@gmail.com>
2022-08-02 11:35:13 +0900

    Fix trigging @sort-keys in array

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -14,7 +14,7 @@ const getConfig = (sourceCode: SourceCode, annotationName: string, commentEndLin
   const matchedCommentLineString = matchedComment.value
     .split('\n')
     .map((lineString) => lineString.trim())
-    .find((lineString) => lineString.startsWith(annotationName))
+    .find((lineString) => lineString.split(' ').includes(annotationName))
 
   if (!matchedCommentLineString) {
     return null
