8c18aa11501c5fa15cdd664c6992c47b6e2a58fb
Ron Park <47266692+ronparkdev@users.noreply.github.com>
2022-10-14 13:32:07 +0900

    Update sort-annotation.ts

--- src/rules/sort-annotation.ts
+++ src/rules/sort-annotation.ts
@@ -24,37 +24,21 @@ export default createRule<Options, MessageIds>({
     },
   },
   defaultOptions: [],
+  ...
+}
+  
+export default createRule<Options, MessageIds>({
+  ...
   create(context) {
     const sourceCode = context.getSourceCode()
 
     return {
       ArrayExpression(node): void {
-        const commentExpectedEndLine = node.loc.start.line - 1
-
-        const config = ConfigUtils.getConfig(sourceCode, '@sort', commentExpectedEndLine)
-
-        if (!config) {
-          return
-        }
-
-        const { isReversed } = config
-
-        const comparer = ComparerUtils.makeArrayValueComparer({ isReversed, sourceCode })
-
-        const sortedElements = [...node.elements].sort(comparer)
-
-        const needSort = ArrayUtils.zip2(node.elements, sortedElements).some(
-          ([element, sortedElement]) => element !== sortedElement,
-        )
-
+        ...
+        
         if (needSort) {
-          const diffRanges = ArrayUtils.zip2(node.elements, sortedElements).map(([from, to]) => ({
-            from: from.range,
-            to: to.range,
-          }))
-
-          const fixedText = FixUtils.getFixedText(sourceCode, node.range, diffRanges)
-
+          ...
+          
           context.report({
             node,
             messageId: 'hasUnsortedKeys',
