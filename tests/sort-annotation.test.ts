4180b02de9de679c60f6e17c7983dafa5f4fe152
Ron Park <47266692+ronparkdev@users.noreply.github.com>
2022-10-14 13:27:39 +0900

    Update sort-annotation.test.ts

--- tests/sort-annotation.test.ts
+++ tests/sort-annotation.test.ts
@@ -22,62 +22,17 @@ ruleTester.run('sort-annotation', rule, {
   valid: [
     {
       code: `
-      const A = 'A'
-      const B = 'B'
-      enum Type {
-        A = 'A',
-        B = 'B',
-      }
       // @sort
-      const array = [false, true, 1, 2, 11, 12, 1n, 2n, 11n, 12n, '1', '11', '2', '22', 'a', 'b', A, B, Type.A, Type.B]
-      `,
-      filename: getFilename('main.ts'),
-    },
-    {
-      code: `
-      const A = 'A'
-      const B = 'B'
-      const C = 'C'
-      enum Type {
-        A = 'A',
-        B = 'B',
-        C = 'C',
-      }
-      // @sort:reversed
-      const reversedArray = [Type.B, Type.A, B, A, 'b', 'a', '22', '2', '11', '1', 12n, 11n, 2n, 1n, 12, 11, 2, 1, true, false]    
-      `,
-      filename: getFilename('main.ts'),
-    },
-    {
-      code: `
-      /*
-        @sort
-      */
-        const simpleArray = [1, 2]
-      `,
-      filename: getFilename('main.ts'),
-    },
-    {
-      code: `
-      /*
-        @sort:reversed
-      */
-        const reversedSimpleArray = [2, 1]
+      const array = [false, true, 1, 2, 11, 12, 1n, 2n, 11n, 12n]
       `,
       filename: getFilename('main.ts'),
     },
   ],
   invalid: [
-    {
+    {{
       code: `
-      const A = 'A'
-      const B = 'B'
-      enum Type {
-        A = 'A',
-        B = 'B',
-      }
       // @sort
-      const array = [Type.B, Type.A, B, A, 'b', 'a', '22', '2', '11', '1', 12n, 11n, 2n, 1n, 12, 11, 2, 1, true, false]
+      const array = [12n, 11n, 2n, 1n, 12, 11, 2, 1, true, false]
       `,
       errors: [
         {
@@ -86,77 +41,8 @@ ruleTester.run('sort-annotation', rule, {
         },
       ],
       output: `
-      const A = 'A'
-      const B = 'B'
-      enum Type {
-        A = 'A',
-        B = 'B',
-      }
       // @sort
-      const array = [false, true, 1, 2, 11, 12, 1n, 2n, 11n, 12n, '1', '11', '2', '22', 'a', 'b', A, B, Type.A, Type.B]
-      `,
-      filename: getFilename('main.ts'),
-    },
-    {
-      code: `
-      const A = 'A'
-      const B = 'B'
-      // @sort:reversed
-      const reversedArray = [false, true, 1, 2, 11, 12, 1n, 2n, 11n, 12n, '1', '11', '2', '22', 'a', 'b', A, B, Type.A, Type.B]
-      `,
-      errors: [
-        {
-          messageId: 'hasUnsortedKeys',
-          type: AST_NODE_TYPES.ArrayExpression,
-        },
-      ],
-      output: `
-      const A = 'A'
-      const B = 'B'
-      // @sort:reversed
-      const reversedArray = [Type.B, Type.A, B, A, 'b', 'a', '22', '2', '11', '1', 12n, 11n, 2n, 1n, 12, 11, 2, 1, true, false]
-      `,
-      filename: getFilename('main.ts'),
-    },
-    {
-      code: `
-      /*
-        @sort
-      */
-      const simpleArray = [2, 1]
-      `,
-      errors: [
-        {
-          messageId: 'hasUnsortedKeys',
-          type: AST_NODE_TYPES.ArrayExpression,
-        },
-      ],
-      output: `
-      /*
-        @sort
-      */
-      const simpleArray = [1, 2]
-      `,
-      filename: getFilename('main.ts'),
-    },
-    {
-      code: `
-      /*
-        @sort:reversed
-      */
-      const reversedSimpleArray = [1, 2]
-      `,
-      errors: [
-        {
-          messageId: 'hasUnsortedKeys',
-          type: AST_NODE_TYPES.ArrayExpression,
-        },
-      ],
-      output: `
-      /*
-        @sort:reversed
-      */
-      const reversedSimpleArray = [2, 1]
+      const array = [false, true, 1, 2, 11, 12, 1n, 2n, 11n, 12n]
       `,
       filename: getFilename('main.ts'),
     },
